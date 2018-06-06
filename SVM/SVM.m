27741b2f74fafaae96090fafe14db9c15b55cf6b
Mateus Mota <33534390+mateuspontesm@users.noreply.github.com>
2018-06-05 23:34:25 -0300

    Update SVM.m

--- SVM/SVM.m
+++ SVM/SVM.m
@@ -6,7 +6,7 @@ load('FisherIris.mat');
 mtData = [FisherIris.SepalLength FisherIris.SepalWidth FisherIris.PetalLength FisherIris.PetalWidth double(FisherIris.Species)]; %conjunto de amostras
 dK = 3 ; %numero de classes 
 dC= 0.4; %C
-dMaxIter=10; %max de iterações sem mudar o alpha
+dMaxIter=10; %max de iteraÃ§Ãµes sem mudar o alpha
 dTol=10^-5;
 [nSamples,nCols]=size(mtData); %numero de amostras total e numero de colunas da matriz
 [TrainInd,~,TestInd]=dividerand(nSamples,0.8,0,0.2);
@@ -17,10 +17,10 @@ mtD=mtTrain(:,end); %dados de saida
 %% Criando as 3 SVMs
 
 mtY1=mtD; mtY2=mtD; mtY3=mtD;
-mtY1(mtY1~=1)=-1; %Mudança dos dados para SVM1
-mtY2(mtY2~=2)=-1; %Mudança dos dados para SVM2
+mtY1(mtY1~=1)=-1; %MudanÃ§a dos dados para SVM1
+mtY2(mtY2~=2)=-1; %MudanÃ§a dos dados para SVM2
 mtY2(mtY2==2)=1;
-mtY3(mtY3~=3)=-1; %Mudança dos dados para SVM3
+mtY3(mtY3~=3)=-1; %MudanÃ§a dos dados para SVM3
 mtY3(mtY3==3)=1;
 [W1,b1]=createSVM(dC,dTol,dMaxIter,mtX,mtY1,'l'); %cria a SVM1
 [W2,b2]=createSVM(dC,dTol,dMaxIter,mtX,mtY2,'l'); %cria a SVM2
@@ -40,7 +40,6 @@ mtY=[mtX*W1'+b1 , mtX*W2'+b2, mtX*W3'+b3];
 I(I~=mtDTest)=0;
 pCT= nnz(I)/length(I);
 fprintf('\nPorcentagem de teste: %6.4f \n',pCT)
-
 
 
 
